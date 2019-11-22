a211ec051edc731fafcd0b26cc11e819f997bd19
khaled zaid <khaledzzzz1992@gmail.com>
2019-11-22 22:10:34 +0100

    Initial commit

---
+++ app/src/main/java/com/kh_sof_dev/tasoug/Login_activity.java
@@ -0,0 +1,14 @@
+package com.kh_sof_dev.tasoug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+    }
+}
