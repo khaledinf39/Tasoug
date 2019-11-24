3612ac3d097ee6814456ac1ff8d7eff0ed12c878
khaled zaid <khaledzzzz1992@gmail.com>
2019-11-24 11:49:28 +0100

    new

---
+++ app/src/main/java/com/kh_sof_dev/tasoug/Activities/Product_details.java
@@ -0,0 +1,16 @@
+package com.kh_sof_dev.tasoug.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kh_sof_dev.tasoug.R;
+
+public class Product_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
